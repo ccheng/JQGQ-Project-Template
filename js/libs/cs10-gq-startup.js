83f53aaa605f53e28e3e020214241708538e1220
C Cheng <ccheng@users.noreply.github.com>
2016-07-27 17:56:32 -0600

    Add CDN lib option but default to local fallback
    Feature switch makes cs10 lib load from CDN instead of
    local fallback. Intended for when hot patching needed.

--- js/libs/cs10-gq-startup.js
+++ js/libs/cs10-gq-startup.js
@@ -15,36 +15,101 @@
  */
 
 $(function () {
-    var loadScript = function (url, callback) {
-        // see also: https://stackoverflow.com/questions/950087/include-a-javascript-file-in-another-javascript-file
+    var localLibsOnly = true; // set false to use CDN libs if available, esp. if hot patching desired
 
+    // Avoid console.log errors in browsers that lack a console (i.e. IE)
+    // see: http://stackoverflow.com/questions/7742781/why-javascript-only-works-after-opening-developer-tools-in-ie-once
+    // for more complete solution, see: https://github.com/h5bp/html5-boilerplate/blob/master/src/js/plugins.js
+    if(!window.console) {
+        console={log: function(){}};
+    }
+    
+    var urlExist = function (url, callback){ // callback is called with whether url exists (boolean)
+        if (localLibsOnly){
+            callback(false);
+            return;
+        }
+    
+        // see: https://stackoverflow.com/questions/3646914/how-do-i-check-if-file-exists-in-jquery-or-javascript
+        var http = new XMLHttpRequest();
+        http.open('HEAD', url, true);
+        http.onload = function(e){
+            if (http.readyState === 4) {
+                callback( http.status!==404 );
+            }
+        }
+        http.onerror = function (e) {
+            callback(false);
+        };
+        http.send(null);
+    };
+
+    var loadCss = function (url, callback) {
+        var head = document.getElementsByTagName('head')[0];
+        var link = document.createElement('link');
+        link.rel = "stylesheet";
+        link.type = 'text/css';
+        link.media = 'all';
+        link.href = url;
+
+        link.onreadystatechange = callback;
+        link.onload = callback;
+
+        head.appendChild(link);
+        //console.log("Loaded css: " + url);
+    };
+
+    var loadScript = function (url, callback) {
+        // see: https://stackoverflow.com/questions/950087/include-a-javascript-file-in-another-javascript-file
         var head = document.getElementsByTagName('head')[0];
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = url;
 
-        // Then bind the event to the callback function.
-        // There are several events for cross browser compatibility.
         script.onreadystatechange = callback;
         script.onload = callback;
 
-        // Fire the loading
         head.appendChild(script);
         //console.log("Loaded script: " + url);
     };
+    
+    var loadRsrc = function(loader, scriptName, cdnUrl, fallbackUrl, callback){
+        // loader must be like loadScript(url, callback), or loadCss(url, callback)
+        callback = callback || function(){};
+        urlExist(cdnUrl, function(theUrlExists){
+            if (theUrlExists){
+                loader(cdnUrl, function(){
+                    console.log('Using online CDN: ' + scriptName);
+                    callback();
+                });
+            } else {
+                loader(fallbackUrl, function(){
+                    console.log('Using fallback lib: ' + scriptName);
+                    callback();
+                });
+            }
+        });
+    };
 
+    // main
     var cs10libname = "cs10-0.7.1-0.4.5.js";
-    loadScript("js/libs/" + cs10libname, function () {
-        consolePrint("Using: " + cs10libname);
+    var gitTag = "v5.7";
+    loadRsrc(loadScript, cs10libname,
+             "https://gitcdn.xyz/repo/ccheng/JQGQ-Project-Template/" + gitTag + "/js/libs/" + cs10libname,
+             "js/libs/" + cs10libname, function(){
+             // GitCDN will take up to 2 hours to propagate file changes
+        //consolePrint("Using: " + cs10libname);
 
-        sprite("playground").playground({height: GQ_PLAYGROUND_HEIGHT,
+        sprite("playground").playground({
+            height: GQ_PLAYGROUND_HEIGHT,
             width: GQ_PLAYGROUND_WIDTH,
             keyTracker: true,
-            mouseTracker: true});
+            mouseTracker: true
+        });
 
         var studentProgramName = "myprogram.js";
         loadScript("js/" + studentProgramName, function () {
-            consolePrint("Running: " + studentProgramName);
+            console.log("Running: " + studentProgramName);
             
             setup();
 
